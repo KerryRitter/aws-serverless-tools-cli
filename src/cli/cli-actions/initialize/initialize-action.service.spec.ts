fc454e02942d87b121c06f822e1bb428cb53fa10
Benjamin Main <bmain@lumeris.com>
2020-07-23 19:11:34 -0500

    refactor(InitializeActionServiceSpec): Fix tests.

--- src/cli/cli-actions/initialize/initialize-action.service.spec.ts
+++ src/cli/cli-actions/initialize/initialize-action.service.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InitializeActionService } from './initialize-action.service';
+import { NpmService, FileSystemService } from '../../../core';
 
 describe('InitializeActionService', () => {
   let service: InitializeActionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [InitializeActionService],
+      providers: [
+        InitializeActionService,
+        {
+          provide: NpmService,
+          useValue: {
+            installPackages: () => null,
+            addScript: () => null,
+            addTopLevelConfig: () => null,
+          }
+        },
+        {
+          provide: FileSystemService,
+          useValue: {
+            createDirectory: () => null,
+            getCwdPath: () => null,
+            writeFile: () => null,
+          }
+        }
+      ],
     }).compile();
 
     service = module.get<InitializeActionService>(InitializeActionService);
